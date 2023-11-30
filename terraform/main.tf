7c7aca4f5daa8b0f5f008f460b1b1227458cc870
João Tedeschi <joaorafaelbt@gmail.com>
2023-11-30 01:53:47 -0300

    Add Google Cloud Storage bucket and Cloud Function resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,82 +38,82 @@ resource "google_pubsub_topic" "default" {
   name = "classify-emails-topic"
 }
 
-# resource "google_storage_bucket" "default" {
-#   name                        = "${random_id.bucket_prefix.hex}-gcf-source" # Every bucket name must be globally unique
-#   location                    = "US"
-#   uniform_bucket_level_access = true
-# }
+resource "google_storage_bucket" "default" {
+  name                        = "${random_id.bucket_prefix.hex}-gcf-source" # Every bucket name must be globally unique
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
 
-# data "archive_file" "default" {
-#   type        = "zip"
-#   output_path = "/tmp/function-source.zip"
-#   source_dir  = "cloud_function/"
-# }
+data "archive_file" "default" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "cloud_function/"
+}
 
-# resource "google_storage_bucket_object" "default" {
-#   name   = "function-source.zip"
-#   bucket = google_storage_bucket.default.name
-#   source = data.archive_file.default.output_path # Path to the zipped function source code
-#   depends_on = [ 
-#     data.archive_file.default,
-#     google_storage_bucket.default
-#     ]
-# }
+resource "google_storage_bucket_object" "default" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.default.output_path # Path to the zipped function source code
+  depends_on = [ 
+    data.archive_file.default,
+    google_storage_bucket.default
+    ]
+}
 
-# resource "google_cloudfunctions2_function" "default" {
-#   name        = "classify-email-gcf"
-#   location    = "us-central1"
-#   description = "Classify emails using genAI from OpenAI"
-#   depends_on = [
-#     google_service_account.default,
+resource "google_cloudfunctions2_function" "default" {
+  name        = "classify-email-gcf"
+  location    = "us-central1"
+  description = "Classify emails using genAI from OpenAI"
+  depends_on = [
+    google_service_account.default,
 
-#     ]
+    ]
   
 
-#   build_config {
-#     runtime     = "python310"
-#     entry_point = "main" # Set the entry point
-#     environment_variables = {
-#       BUILD_CONFIG_TEST = "build_test"
-#     }
-#     source {
-#       storage_source {
-#         bucket = google_storage_bucket.default.name
-#         object = google_storage_bucket_object.default.name
-#       }
-#     }
-#   }
+  build_config {
+    runtime     = "python310"
+    entry_point = "main" # Set the entry point
+    environment_variables = {
+      BUILD_CONFIG_TEST = "build_test"
+    }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.default.name
+      }
+    }
+  }
 
-#   service_config {
-#     max_instance_count = 3
-#     min_instance_count = 1
-#     available_memory   = "512M"
-#     timeout_seconds    = 60
-#     environment_variables = {
-#       SERVICE_CONFIG_TEST = "config_test"
-#     }
-#     # ingress_settings               = "ALLOW_INTERNAL_ONLY"
-#     all_traffic_on_latest_revision = true
-#     service_account_email = google_service_account.default.email
-#   }
+  service_config {
+    max_instance_count = 3
+    min_instance_count = 1
+    available_memory   = "512M"
+    timeout_seconds    = 60
+    environment_variables = {
+      SERVICE_CONFIG_TEST = "config_test"
+    }
+    # ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email = google_service_account.default.email
+  }
 
-#   event_trigger {
-#     trigger_region = "us-central1"
-#     event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
-#     pubsub_topic   = google_pubsub_topic.default.id
-#     retry_policy   = "RETRY_POLICY_RETRY"
-#   }
+  event_trigger {
+    trigger_region = "us-central1"
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.default.id
+    retry_policy   = "RETRY_POLICY_RETRY"
+  }
 
 
-# }
+}
 
-# resource "google_cloud_run_service_iam_member" "member" {
-#   location = google_cloudfunctions2_function.default.location
-#   service  = google_cloudfunctions2_function.default.name
-#   role     = "roles/run.invoker"
-#   member   = "allUsers"
-# }
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.default.location
+  service  = google_cloudfunctions2_function.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
 
-# output "function_uri" {
-#   value = google_cloudfunctions2_function.default.service_config[0].uri
-# }
+output "function_uri" {
+  value = google_cloudfunctions2_function.default.service_config[0].uri
+}
